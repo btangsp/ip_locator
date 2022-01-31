7732176fb4b256b26c273149037254175fd6e206
Brian Tang <btang.tsp@gmail.com>
2022-01-30 20:49:51 -0600

    implemented user input func

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [input, setInput] = useState("");
+
+  const handleChange = (val) => {
+    console.log(val);
+    setInput(val);
+  }
+
+  const handleClick = (e) => {
+    setInput("");
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,8 +21,8 @@ const App = () => {
       </header>
       <main>
         <form>
-          <input type="text" id="ip" name="ip" />
-          <input type="submit" value="Submit" />
+          <input type="text" id="ip" name="ip" value={input} onChange={e => handleChange(e.target.value)} />
+          <input type="submit" value="Submit" onClick={e => handleClick(e)} />
         </form>
         <div>
           <p>Latitude:</p>
